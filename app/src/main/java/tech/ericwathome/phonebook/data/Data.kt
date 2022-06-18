6b3227e89666dae53252d341084f49ac77941623
Eric Wafula <wathomeeric@gmail.com>
2022-06-19 00:58:28 +0300

    added contactData singleton

--- app/src/main/java/tech/ericwathome/phonebook/data/Data.kt
+++ app/src/main/java/tech/ericwathome/phonebook/data/Data.kt
@@ -1,3 +1,22 @@
 package tech.ericwathome.phonebook.data
 
-data class Contact (var firstName: String, var lastName: String, var phoneNumber: String)
+data class Contact (var firstName: String, var lastName: String, var phoneNumber: String, var isFavorite: Boolean = false)
+
+object contactData {
+    var contacts = ArrayList<Contact>()
+
+    init {
+        initializeContacts()
+    }
+
+    private fun initializeContacts() {
+        var contact = Contact("Eric", "Wathome", "0722261627")
+        contacts.add(contact)
+        contact = Contact("Daniel", "Eric", "0705189393")
+        contacts.add(contact)
+        contact = Contact("Eric", "Wafula", "0712250082")
+        contacts.add(contact)
+        contact = Contact("Leon", "Salinski", "0722261627")
+        contacts.add(contact)
+    }
+}
