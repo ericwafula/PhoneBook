fd11d4e6ef8c1193ebcce8624881b916fcd01271
Eric Wafula <wathomeeric@gmail.com>
2022-06-21 14:26:29 +0300

    added add to favorites functionality

--- app/src/main/java/tech/ericwathome/phonebook/adapter/AllContactsAdapter.kt
+++ app/src/main/java/tech/ericwathome/phonebook/adapter/AllContactsAdapter.kt
@@ -2,11 +2,13 @@ package tech.ericwathome.phonebook.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import tech.ericwathome.phonebook.R
 import tech.ericwathome.phonebook.data.Contact
+import tech.ericwathome.phonebook.data.ContactData
 import tech.ericwathome.phonebook.databinding.ContactListItemBinding
 
 class AllContactsAdapter (private val context: Context, private val contacts: ArrayList<Contact>) : RecyclerView.Adapter<AllContactsAdapter.ViewHolder>() {
@@ -19,15 +21,18 @@ class AllContactsAdapter (private val context: Context, private val contacts: Ar
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val contact = contacts[position]
         holder.setData(contact, position)
+        holder.setListeners()
     }
 
     override fun getItemCount(): Int {
         return contacts.size
     }
 
-    inner class ViewHolder(private val binding: ContactListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ContactListItemBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
         private var currentPosition = -1
         private lateinit var currentContact: Contact
+        private val starFilled = ResourcesCompat.getDrawable(context.resources, R.drawable.ic_favorite_filled, null)
+        private val starOutlined = ResourcesCompat.getDrawable(context.resources, R.drawable.ic_favorites_outline, null)
 
         fun setData(contact: Contact, position: Int) {
             currentPosition = position
@@ -38,11 +43,29 @@ class AllContactsAdapter (private val context: Context, private val contacts: Ar
             binding.txvPhone.text = contact.phoneNumber
 
             if (contact.isFavorite) {
-                binding.imvFavorite.setImageDrawable(ResourcesCompat.getDrawable(context.resources, R.drawable.ic_favorite_filled, null))
+                binding.imvFavorite.setImageDrawable(starFilled)
             } else {
-                binding.imvFavorite.setImageDrawable(ResourcesCompat.getDrawable(context.resources, R.drawable.ic_favorites_outline, null))
+                binding.imvFavorite.setImageDrawable(starOutlined)
             }
         }
 
+        fun setListeners() {
+            binding.imvFavorite.setOnClickListener(this@ViewHolder)
+        }
+
+        override fun onClick(v: View?) {
+            when (v?.id) {
+                R.id.imv_favorite -> addToFavorites()
+            }
+        }
+
+        private fun addToFavorites() {
+            currentContact.isFavorite = !currentContact.isFavorite
+            if (currentContact.isFavorite) {
+                binding.imvFavorite.setImageDrawable(starFilled)
+            } else {
+                binding.imvFavorite.setImageDrawable(starOutlined)
+            }
+        }
     }
 }
