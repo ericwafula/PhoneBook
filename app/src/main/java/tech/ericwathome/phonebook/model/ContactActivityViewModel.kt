83a9fedce6950641a1bda029f16a039ec5303d42
Eric Wafula <wathomeeric@gmail.com>
2022-06-22 10:14:08 +0300

    added logic to save a contact

--- app/src/main/java/tech/ericwathome/phonebook/model/ContactActivityViewModel.kt
+++ app/src/main/java/tech/ericwathome/phonebook/model/ContactActivityViewModel.kt
@@ -1,6 +1,16 @@
 package tech.ericwathome.phonebook.model
 
 import androidx.lifecycle.ViewModel
+import tech.ericwathome.phonebook.data.Contact
+import tech.ericwathome.phonebook.data.ContactData
 
 class ContactActivityViewModel : ViewModel() {
+    private val tag = ContactActivityViewModel::class.simpleName
+    private lateinit var currentContact: Contact
+
+    fun saveContact(contact: Contact) {
+        currentContact = contact
+
+        ContactData.contacts.add(contact)
+    }
 }
--- app/src/main/java/tech/ericwathome/phonebook/ui/activities/ContactActivity.kt
+++ app/src/main/java/tech/ericwathome/phonebook/ui/activities/ContactActivity.kt
@@ -2,11 +2,50 @@ package tech.ericwathome.phonebook.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import tech.ericwathome.phonebook.R
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import tech.ericwathome.phonebook.data.Contact
+import tech.ericwathome.phonebook.data.ContactData
+import tech.ericwathome.phonebook.databinding.ActivityContactBinding
+import tech.ericwathome.phonebook.model.ContactActivityViewModel
 
 class ContactActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityContactBinding
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[ContactActivityViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact)
+        binding = ActivityContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener { saveContact() }
     }
+
+    override fun onPause() {
+        super.onPause()
+        saveContact()
+    }
+
+    private fun saveContact() {
+        val firstName = binding.txvFirstName.text.toString().trim()
+        val lastName = binding.txvLastName.text.toString().trim()
+        val phoneNumber = binding.txvPhoneNumber.text.toString().trim()
+
+        if (firstName.isNotEmpty() && lastName.isNotEmpty() && phoneNumber.isNotEmpty()) {
+            val contact = Contact(firstName, lastName, phoneNumber)
+            if (!ContactData.contacts.contains(contact)) {
+                viewModel.saveContact(contact)
+            } else {
+                Snackbar.make(binding.root, "contact already exists", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            Snackbar.make(binding.root, "all fields must contain a value", Snackbar.LENGTH_LONG)
+                .show()
+        }
+    }
+
 }
